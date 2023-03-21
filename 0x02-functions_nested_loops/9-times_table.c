2d4e8cb5cc775402e8f5e0270c03e02a9646ca49
CASANOVA666 <boualiamino0123@gmail.com>
2023-03-21 11:31:06 -0700

    dd

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints the 9 times table.
+ *
+ * Return: Always 0.
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			_putchar((i * j) + 48);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
