8243af6c395309a38bc74df2650896f9055370b4
CASANOVA666 <boualiamino0123@gmail.com>
2023-03-21 07:13:18 -0700

    betty is shity

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _islower - function that checks for lowercase character.
+ * _islower - function that checks for lowercase character with 'c' parameter .
  *
- * Return: Always 0.
+ * Return: Always 0 or 1 .
  */
 
 int _islower(int c)
