61e2e71e07fc28acee4a80692415449c141176f6
CASANOVA666 <boualiamino0123@gmail.com>
2023-03-21 07:46:18 -0700

    second

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int print_sign - function that prints the sign of a number.
+ *print_sign - function that prints the sign of a number.
  *@n : integer to check
  * Return: Always 0 or 1 or -1 .
  */
@@ -13,7 +13,7 @@ int print_sign(int n)
 		return (1);
 		_putchar(43);
 	}
-	else if (n = 0)
+	else if (n == 0)
 	{
 		return (0);
 		_putchar(48);
