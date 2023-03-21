cba13d6c10fb2509b29c985a77f2bc181e6c0979
CASANOVA666 <boualiamino0123@gmail.com>
2023-03-21 14:58:29 -0700

    f

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,15 @@
 
 /**
  *add - function that adds two integers and returns the result
- *@a ,@b: integers to check
+ *@a: integer to check
+ *@b: integer to check
  *Return: Always result .
  */
 
 int add(int a, int b)
 {
 	int r;
+
 	r = a + b;
 	return (r);
 }
