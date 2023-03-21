236c1cc2e33955630e9e546833c31a57cbf50852
CASANOVA666 <boualiamino0123@gmail.com>
2023-03-21 11:49:19 -0700

    m

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,14 +19,16 @@ void times_table(void)
 			{
 				_putchar(r + '0');
 				_putchar(',');
-				_putchar('  ');
+				_putchar(' ');
+				_putchar(' ');
 			}
 			else
 			{
 				_putchar(r / 10 + '0');
 				_putchar(r % 10 + '0');
 				_putchar(',');
-				_putchar('  ');
+				_putchar(' ');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
