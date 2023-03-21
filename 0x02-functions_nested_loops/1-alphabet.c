e8a0a6260aaba4cba460ccad1da5bd6d7cb9ce1d
CASANOVA666 <boualiamino0123@gmail.com>
2023-03-21 05:46:40 -0700

    first try

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	void print_alphabet(void)
+	{
+		char c;
+		for (c = 97; c <= 122; c++)
+			_putchar(c);
+		_putchar('\n');
+		return (0);
+	}
+	return (0);
+}
