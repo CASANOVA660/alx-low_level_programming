621a6b4c57c1aa7206df7ec3d06b3eca4af75bc0
CASANOVA666 <boualiamino0123@gmail.com>
2023-03-21 17:08:20 -0700

    l

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,12 +10,12 @@ int main(void)
 {
 	int i, sm;
 
-	for(i = 3; i < 1025; i++)
+	for (i = 3; i < 1025; i++)
 	{
-		if((i % 5 == 0) || (i % 3 == 0))
+		if ((i % 5 == 0) || (i % 3 == 0))
 			sm = sm + i;
 	}
-	printf("%d",sum);
+	printf("%d", sm);
 	printf("\n");
 	return (0);
 }
