0111f1d403f81d68efd4080205288455f9ddcdb7
CASANOVA666 <boualiamino0123@gmail.com>
2023-03-23 06:55:14 -0700

    add task 4 file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers 0 to 9 without 2 4.
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	char a;
+
+	for (a = 48; a < 58; a++)
+	{
+		if (a == 50 || a == 52)
+			a++;
+		else
+			_putchar(a);
+	}
+	_putchar('\n');
+}
