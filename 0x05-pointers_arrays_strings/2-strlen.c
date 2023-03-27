c0a0d18c8b1c0dae0d10d45967315d6dcf94fc27
CASANOVA666 <boualiamino0123@gmail.com>
2023-03-27 14:22:21 -0700

    add task2 file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: int to check
+ * Return: Always int.
+ */
+
+int _strlen(char *s)
+{
+	int str = 0;
+
+	while (*s != '\0')
+	{
+		str++;
+		*s++;
+	}
+	return (str);
+}
