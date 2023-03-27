9f5f82975ddb7afa454c8184489e741b27fe8aa5
CASANOVA666 <boualiamino0123@gmail.com>
2023-03-27 15:29:24 -0700

    add task 5 file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ * @s: string to check.
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char tmp;
+
+	while (*s != '\0')
+	{
+		len++;
+	}
+	for (; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = tmp;
+	}
+}
