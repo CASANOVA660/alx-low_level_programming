bb3f0e3f3565f2e5bd24db3b348e1bdc74ba3202
CASANOVA666 <boualiamino0123@gmail.com>
2023-04-05 06:54:07 -0700

    add task1 file

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,12 +9,13 @@
 int _strlen_recursion(char *s)
 {
 	int len = 0;
+
 	if (*s == '\0')
 		return (0);
 	else
 	{
-		_strlen_recursion(s + 1);
 		len++;
+		_strlen_recursion(s + 1);
 	}
 
 	return (len);
