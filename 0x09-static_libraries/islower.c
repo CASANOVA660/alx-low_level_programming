b9fc5b3a3323cc63e4af616ec4fca51fd12f91d3
CASANOVA666 <boualiamino0123@gmail.com>
2023-04-07 12:16:39 -0700

    testting the static library

---
+++ 0x09-static_libraries/islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - function that checks for lowercase character with 'c' parameter .
+ *@c: character to check
+ * Return: Always 0 or 1 .
+ */
+
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+		return (1);
+	else
+		return (0);
+}
