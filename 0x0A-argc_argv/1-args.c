acaf041ccd343548db77deb18563a0baecadde76
CASANOVA666 <boualiamino0123@gmail.com>
2023-04-10 12:19:57 -0700

    add args file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,7 @@
  * Return: always 0.
  */
 
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, __attribute__((unused)) char *argv[])
 {
 	printf("%d\n", argc);
 	return (0);
