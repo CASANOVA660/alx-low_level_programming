b67fea1535a545c501def71c4bea51b6522f0a56
CASANOVA666 <boualiamino0123@gmail.com>
2023-04-10 12:04:50 -0700

    add README.md file and task 0 file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* main - main function with 2 arguments
+ * @argc: argument count
+ * @argv: array of strings
+ * Return always 0
+ */
+
+
+int main (int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s",argv[0]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+this project is about the main arguments argv and argc 
