beeaab700b60b01805f115d41c3a7c9a74c54fa8
CASANOVA666 <boualiamino0123@gmail.com>
2023-04-10 13:17:33 -0700

    add task 3 file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,8 +14,6 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-	result = num1 * num2;
-
 	if (argc != 3)
 	{
 		printf("Error\n");
@@ -23,6 +21,7 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
+		result = num1 * num2;
 		printf("%d\n", result);
 	}
 	return (0);
