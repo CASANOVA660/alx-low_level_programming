dec728c3d42e3969ea2ec77231d2c62eabcde374
CASANOVA666 <boualiamino0123@gmail.com>
2023-04-10 13:52:20 -0700

    add task 4 file

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,10 +10,11 @@
  * Return: 1 if are alphabetic arguments otherwise 0.
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int add = 0, i, j;
+	int sum, i, j;
 
+	sum = 0;
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
@@ -26,6 +27,7 @@ int main(int argc, char *argv[])
 		}
 		sum += atoi(argv[i]);
 	}
-	printf("%d\n", add);
+
+	printf("%d\n", sum);
 	return (0);
 }
