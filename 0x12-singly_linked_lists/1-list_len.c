ed0c03fc567b52a6ef33d5b92bdb6caaf97e3e64
CASANOVA666 <boualiamino0123@gmail.com>
2023-05-01 13:23:03 -0700

    fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,22 @@
-#include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "lists.h"
 
 /**
- * list_len - function that returns the number of element.
- * @h: pointer
+ * list_len - function that returns the number of elements in a linked
+ * list_t list
+ * @h: header pointer
  * Return: number of elements
  */
 
 size_t list_len(const list_t *h)
 {
-	size_t  nb = 0;
+	size_t cont = 0;
 
 	while (h != NULL)
 	{
 		h = h->next;
-		nb++;
+		cont++;
 	}
-	return (nb);
+	return (cont);
 }
